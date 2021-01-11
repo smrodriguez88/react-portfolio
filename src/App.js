ef414c8387167a28e48ba0de895a8f5ef487c2c3
Steve Rodriguez <smrodriguez88@gmail.com>
2021-01-11 00:15:01 -0600

    Fixed base url being different for deploy, fixed homepage URL in package.json

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <Navbar />
             <Switch>
               <Route exact path="/" component={AboutMe} />
